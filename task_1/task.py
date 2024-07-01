7deff8c369a82c02dc2d362b4ed26d72a77e52f1
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-01 16:19:57 +0000

    done

---
+++ task_1/task.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask, request
+""" Returns an api response  with user name, ip and temperature """
+
+app = Flask(__name__)
+
+@app.route("/api/hello", methods=['GET'])
+def hello():
+    """ main api endpoint for request """
+    username = request.args.get('visitor_name')
+
+    client_ip = request.remote_addr
+    return (client_ip)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ Handle requests to other endpoints """
+    return jsonify({"status": 404, "message": "Not Found"}), 404
+
+if __name__ == "__main__":
+    app.run(DEBUG=True)
