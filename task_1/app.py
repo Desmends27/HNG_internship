9ff394795655e0bc82c9eb138399c4be83a0e7f8
Desmends27 <desmondopokumends2004@gmail.com>
2024-07-01 17:50:44 +0000

    test for production

--- task_1/app.py
+++ task_1/app.py
@@ -1,11 +1,12 @@
 import requests
 from flask import Flask, request, jsonify
 import os
-from dotenv import load_dotenv, dotenv_values 
-load_dotenv() 
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
 """ Returns an api response  with user name, ip and temperature """
 
 app = Flask(__name__)
+
 
 @app.route("/api/hello", methods=['GET'])
 def hello():
@@ -14,7 +15,7 @@ def hello():
 
     client_ip = request.remote_addr
     ip_info_url = f"http://ip-api.com/json/{client_ip}"
-    #Request for ip address
+    # Request for ip address
     try:
         response = requests.get(ip_info_url).json()
     except requests.RequestException as e:
@@ -27,16 +28,24 @@ def hello():
         headers = {"accept": "application/json"}
         url = "https://api.tomorrow.io/v4/weather/realtime"
         response = requests.get(url, headers=headers, params=payload).json()
+        temperature = response["temperature"]
     except requests.RequestException as e:
         return jsonify({"error": "Could not retrieve location data"}), 500
     
-    return (response)
+    greeting = f"Hello, {username}!, the temperature is {temperature} degrees Celcius in {location}"
+
+    return jsonify({
+        "client_ip": client_ip,
+        "location": location,
+        "greeting": greeting,
+    })
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     """ Handle requests to other endpoints """
     return jsonify({"status": 404, "message": "Not Found"}), 404
+
 
 if __name__ == "__main__":
     app.run(DEBUG=True)
